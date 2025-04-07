777ca7a9806f38f3441db9a169ed72b6994b6cdf
OIDOS12 <160237783+OIDOS12@users.noreply.github.com>
2025-04-07 15:43:34 +0300

    Add files via upload

---
+++ lab4/work/@lab4_@d@d@n@f2/_primary.vhd
@@ -0,0 +1,10 @@
+library verilog;
+use verilog.vl_types.all;
+entity Lab4_DDNF2 is
+    port(
+        x1              : in     vl_logic;
+        x2              : in     vl_logic;
+        x3              : in     vl_logic;
+        f               : out    vl_logic
+    );
+end Lab4_DDNF2;
