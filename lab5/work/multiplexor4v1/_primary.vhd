0b7d1cee5ba3a554805dc53bafcff8d87560a040
OIDOS12 <160237783+OIDOS12@users.noreply.github.com>
2025-05-04 15:55:54 +0300

    Added new 5 lab

--- lab5/work/multiplexor4v1/_primary.vhd
+++ lab5/work/multiplexor4v1/_primary.vhd
@@ -2,12 +2,12 @@ library verilog;
 use verilog.vl_types.all;
 entity multiplexor4v1 is
     port(
+        D0              : in     vl_logic_vector(7 downto 0);
+        D1              : in     vl_logic_vector(7 downto 0);
+        D2              : in     vl_logic_vector(7 downto 0);
+        D3              : in     vl_logic_vector(7 downto 0);
         A1              : in     vl_logic;
         A0              : in     vl_logic;
-        D0              : in     vl_logic;
-        D1              : in     vl_logic;
-        D2              : in     vl_logic;
-        D3              : in     vl_logic;
-        Q               : out    vl_logic
+        Q               : out    vl_logic_vector(7 downto 0)
     );
 end multiplexor4v1;
